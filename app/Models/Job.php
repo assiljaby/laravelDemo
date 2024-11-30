aef9376d5e6664c777044997be5798fb21547b27
Assil Jaby <assiljaby@Assils-MacBook-Pro.local>
2024-11-30 22:02:35 +0300

    feat: move to eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,39 +2,10 @@
 
 namespace App\Models;
 
-use phpDocumentor\Reflection\Types\Static_;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ];
-    }
-
-    public static function find(int $id): array {
-
-        $job = \Illuminate\Support\Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (!$job) {
-            abort(404, 'Job not found');
-        }
-
-        return $job;
-    }
+        protected $table = 'jobs_listing';
+        protected $fillable = ['title', 'salary'];
 }
--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -3,9 +3,9 @@
         Jobs Listing
     </x-slot:heading>
 
-    <h2 class="font-bold">{{$job['title']}}</h2><br>
+    <h2 class="font-bold">{{$job->title}}</h2><br>
 
     <p>
-        This job pays {{$job['salary']}} a year.
+        This job pays {{$job->salary}} a year.
     </p>
 </x-layout>
