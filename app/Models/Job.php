2e791e54085dc57704a36bfe121405443e7099a1
Assil Jaby <assiljaby@Assils-MacBook-Pro.local>
2024-12-01 01:44:27 +0300

    feat: add relationship between employers and jobs

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,4 +11,9 @@ class Job extends Model
 
     protected $table = 'jobs_listing';
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
