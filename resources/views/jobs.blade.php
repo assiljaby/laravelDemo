eac8a7421a11cd931293aada739993dd5049f4a0
Assil Jaby <altf4irl@gmail.com>
2024-12-04 02:36:41 +0300

    feat: add pagination

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,10 +3,19 @@
         Jobs Listing
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-        <li class="hover:underline text-blue-500"><a href="/job/{{$job['id']}}"><strong class="bold">{{$job['title']}}:</strong> Pays {{$job['salary']}}</a></li>
+        <a href="/job/{{$job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+            <div class="font-bold text-blue-500 text-sm"> {{ $job->employer->name }} </div>
+            <div>
+                <strong class="bold">{{$job['title']}}:</strong> Pays {{$job['salary']}}
+            </div>
+        </a>
         @endforeach
-    </ul>
+
+        <div>
+            {{ $jobs->links() }}
+        </div>
+    </div>
 
 </x-layout>
