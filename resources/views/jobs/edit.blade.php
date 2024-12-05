b5fcbd4eb7b0082fbb500d9b773530a45e926f1e
Assil Jaby <altf4irl@gmail.com>
2024-12-05 13:57:13 +0300

    feat: add delete job

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -57,10 +57,26 @@
                 </div>
             </div>
         </div>
+        <div class="mt-6 flex items-center justify-between gap-x-6">
+            <div class="flex items-center">
+                <button form="delete-form" class="text-red-500 text-sm font-bold">Delete</button>
+            </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
-            <a href="/jobs" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
-            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+            <div class="flex items-center gap-x-6">
+                <a href="/jobs/{{ $job->id }}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+
+                <div>
+                    <button type="submit"
+                            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        Update
+                    </button>
+                </div>
+            </div>
         </div>
+    </form>
+
+    <form id="delete-form" method="POST" action="/job/{{ $job->id }}" class="hidden">
+        @csrf
+        @method('DELETE')
     </form>
 </x-layout>
