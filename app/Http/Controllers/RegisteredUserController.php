dc8762a62341852de289928b34595468a116ef55
Assil Jaby <altf4irl@gmail.com>
2024-12-07 23:39:34 +0300

    feat: add login/register auth

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
@@ -11,6 +14,16 @@ class RegisteredUserController extends Controller
     }
 
     public function store(Request $request) {
-        dd($request->all());
+        $args = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(8), 'confirmed'],
+        ]);
+
+        $user = User::create($args);
+
+        Auth::login($user);
+
+        return redirect('/jobs');
     }
 }
--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -11,6 +13,25 @@ class SessionController extends Controller
     }
 
     function store(Request $request){
-        dd($request->all());
+        $attrs = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (!Auth::attempt($attrs)){
+            throw ValidationException::withMessages([
+                'password' => 'The provided credentials do not match our records.'
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect('/jobs');
+    }
+
+    function destroy(){
+        Auth::logout();
+
+        return redirect('/');
     }
 }
